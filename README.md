65a12f58c0a20a064e1905a18f3c5b9a75fd56a4
Ian McBurnie <imcburnie@ebay.com>
2015-12-11 11:18:16 -0800

    Moved code from internal repo

--- README.md
+++ README.md
@@ -1,2 +1,109 @@
-# jquery-common-keys
-jQuery collection plugin that triggers events for common accessibility keys e.g. ENTER, SPACE, ESC, ARROW KEYS.
+# @ebay/jquery-common-keys
+
+<p>
+    <a href="https://travis-ci.org/ianmcburnie/jquery-common-keys"><img src="https://api.travis-ci.org/ianmcburnie/jquery-common-keys.svg?branch=master" alt="Build Status" /></a>
+    <a href='https://coveralls.io/github/ianmcburnie/jquery-common-keys?branch=master'><img src='https://coveralls.io/repos/ianmcburnie/jquery-common-keys/badge.svg?branch=master&service=github' alt='Coverage Status' /></a>
+</p>
+
+jQuery collection plugin that triggers events for common accessibility keys, e.g. enter, space, esc, arrows.
+
+```js
+$(collection).commonKeys();
+```
+
+## Experimental
+
+This plugin is still in an experimental state, until it reaches v1.0.0 you must consider all minor releases as breaking changes. Patch releases may introduce new features, but will be backwards compatible.
+
+Please use the tilde range specifier in your package.json to pin to a fixed major and minor version.
+
+## Install
+
+```js
+npm install @ebay/jquery-common-keys --save
+```
+
+## Example
+
+```html
+<div tabindex="0" role="button">Fake Button</div>
+
+$('div[role=button]').commonKeys().on('space.commonKeyDown enter.commonKeyDown', function(e) {
+    // activate fake button
+});
+```
+
+*NOTE:* this example is for demo purposes only. Never use a `div` tag for a button!
+
+## Events
+
+* `enter.commonKeyDown`
+* `escape.commonKeyDown`
+* `space.commonKeyDown`
+* `pageup.commonKeyDown`
+* `pagedown.commonKeyDown`
+* `end.commonKeyDown`
+* `home.commonKeyDown`
+* `leftarrow.commonKeyDown`
+* `uparrow.commonKeyDown`
+* `rightarrow.commonKeyDown`
+* `downarrow.commonKeyDown`
+
+* `enter.commonKeyUp`
+* `escape.commonKeyUp`
+* `space.commonKeyUp`
+* `pageup.commonKeyUp`
+* `pagedown.commonKeyUp`
+* `end.commonKeyUp`
+* `home.commonKeyUp`
+* `leftarrow.commonKeyUp`
+* `uparrow.commonKeyUp`
+* `rightarrow.commonKeyUp`
+* `downarrow.commonKeyUp`
+
+## Constants
+
+```js
+$.fn.commonKeys.keyCodes = {
+    ENTER: 13,
+    ESCAPE: 27,
+    SPACE: 32,
+    PAGEUP: 33,
+    PAGEDOWN: 34,
+    END: 35,
+    HOME: 36,
+    LEFTARROW: 37,
+    UPARROW: 38,
+    RIGHTARROW: 39,
+    DOWNARROW: 40
+};
+```
+
+## Development
+
+Run `npm start` for test driven development. All tests are located in `test.js`.
+
+Execute `npm run` to view all available CLI scripts:
+
+* `npm start` test driven development: watches code and re-tests after any change
+* `npm test` runs tests & generates reports (see reports section below)
+* `npm run lint` lints code and reports to jshint.txt
+* `npm run minify` builds minified version of code
+* `npm run build` cleans, lints, tests and minifies (called on `npm prepublish` hook)
+* `npm run clean` deletes all generated test reports and coverage files
+
+## Reports
+
+Each test run will generate the following reports:
+
+* `/test_reports/coverage` contains Istanbul code coverage report
+* `/test_reports/html` contains HTML test report
+* `/test_reports/junit` contains JUnit test report
+
+## CI Build
+
+https://travis-ci.org/ianmcburnie/jquery-common-keys
+
+## Code Coverage
+
+https://coveralls.io/github/ianmcburnie/jquery-common-keys?branch=master
---
+++ jquery.commonkeys.js
@@ -0,0 +1,164 @@
+/**
+* @function $.fn.commonKeys
+* @name jquery-common-keys
+* @version 0.3.2
+* @author Ian McBurnie <ianmcburnie@hotmail.com>
+* @desc jQuery collection plugin that triggers events for common accessibility
+* keys e.g. ENTER, SPACE, ESCAPE, ARROW KEYS.
+* @fires {object} enter.commonKeyDown
+* @fires {object} escape.commonKeyDown
+* @fires {object} space.commonKeyDown
+* @fires {object} pageup.commonKeyDown
+* @fires {object} pagedown.commonKeyDown
+* @fires {object} end.commonKeyDown
+* @fires {object} home.commonKeyDown
+* @fires {object} leftarrow.commonKeyDown
+* @fires {object} uparrow.commonKeyDown
+* @fires {object} downarrow.commonKeyDown
+* @fires {object} rightarrow.commonKeyDown
+*/
+(function ($, window, document, undefined) {
+
+    var pluginName = 'jquery-common-keys';
+
+    var normalizeEvent = function(type, e) {
+        return $.Event(type, { originalEvent: e });
+    };
+
+    $.fn.commonKeys = function commonKeys() {
+
+        return this.each(function onEach() {
+
+            // check element does not already have this plugin
+            if (!$.data(this, pluginName)) {
+
+                jQuery.data(this, pluginName, 'true');
+
+                var $this = $(this),
+                    keyCodes = $.fn.commonKeys.keyCodes;
+
+                var onKeyDown = function(e) {
+                    switch(e.keyCode) {
+                        case keyCodes.ENTER:
+                            $this.trigger(normalizeEvent('enter.commonKeyDown', e));
+                            /* istanbul ignore next */
+                            break;
+                        case keyCodes.ESCAPE:
+                            $this.trigger(normalizeEvent('escape.commonKeyDown', e));
+                            /* istanbul ignore next */
+                            break;
+                        case keyCodes.SPACE:
+                            $this.trigger(normalizeEvent('space.commonKeyDown', e));
+                            /* istanbul ignore next */
+                            break;
+                        case keyCodes.PAGEUP:
+                            $this.trigger(normalizeEvent('pageup.commonKeyDown', e));
+                            /* istanbul ignore next */
+                            break;
+                        case keyCodes.PAGEDOWN:
+                            $this.trigger(normalizeEvent('pagedown.commonKeyDown', e));
+                            /* istanbul ignore next */
+                            break;
+                        case keyCodes.END:
+                            $this.trigger(normalizeEvent('end.commonKeyDown', e));
+                            /* istanbul ignore next */
+                            break;
+                        case keyCodes.HOME:
+                            $this.trigger(normalizeEvent('home.commonKeyDown', e));
+                            /* istanbul ignore next */
+                            break;
+                        case keyCodes.LEFTARROW:
+                            $this.trigger(normalizeEvent('leftarrow.commonKeyDown', e));
+                            /* istanbul ignore next */
+                            break;
+                        case keyCodes.UPARROW:
+                            $this.trigger(normalizeEvent('uparrow.commonKeyDown', e));
+                            /* istanbul ignore next */
+                            break;
+                        case keyCodes.RIGHTARROW:
+                            $this.trigger(normalizeEvent('rightarrow.commonKeyDown', e));
+                            /* istanbul ignore next */
+                            break;
+                        case keyCodes.DOWNARROW:
+                            $this.trigger(normalizeEvent('downarrow.commonKeyDown', e));
+                            /* istanbul ignore next */
+                            break;
+                        /* istanbul ignore next */
+                        default:
+                            break;
+                    }
+                };
+
+                var onKeyUp = function(e) {
+                    switch(e.keyCode) {
+                        case keyCodes.ENTER:
+                            $this.trigger(normalizeEvent('enter.commonKeyUp', e));
+                            /* istanbul ignore next */
+                            break;
+                        case keyCodes.ESCAPE:
+                            $this.trigger(normalizeEvent('escape.commonKeyUp', e));
+                            /* istanbul ignore next */
+                            break;
+                        case keyCodes.SPACE:
+                            $this.trigger(normalizeEvent('space.commonKeyUp', e));
+                            /* istanbul ignore next */
+                            break;
+                        case keyCodes.PAGEUP:
+                            $this.trigger(normalizeEvent('pageup.commonKeyUp', e));
+                            /* istanbul ignore next */
+                            break;
+                        case keyCodes.PAGEDOWN:
+                            $this.trigger(normalizeEvent('pagedown.commonKeyUp', e));
+                            /* istanbul ignore next */
+                            break;
+                        case keyCodes.END:
+                            $this.trigger(normalizeEvent('end.commonKeyUp', e));
+                            /* istanbul ignore next */
+                            break;
+                        case keyCodes.HOME:
+                            $this.trigger(normalizeEvent('home.commonKeyUp', e));
+                            /* istanbul ignore next */
+                            break;
+                        case keyCodes.LEFTARROW:
+                            $this.trigger(normalizeEvent('leftarrow.commonKeyUp', e));
+                            /* istanbul ignore next */
+                            break;
+                        case keyCodes.UPARROW:
+                            $this.trigger(normalizeEvent('uparrow.commonKeyUp', e));
+                            /* istanbul ignore next */
+                            break;
+                        case keyCodes.RIGHTARROW:
+                            $this.trigger(normalizeEvent('rightarrow.commonKeyUp', e));
+                            /* istanbul ignore next */
+                            break;
+                        case keyCodes.DOWNARROW:
+                            $this.trigger(normalizeEvent('downarrow.commonKeyUp', e));
+                            /* istanbul ignore next */
+                            break;
+                        /* istanbul ignore next */
+                        default:
+                            break;
+                    }
+                }
+
+                $this.on('keydown', onKeyDown);
+                $this.on('keyup', onKeyUp);
+            }
+        });
+    };
+
+    $.fn.commonKeys.keyCodes = {
+        ENTER: 13,
+        ESCAPE: 27,
+        SPACE: 32,
+        PAGEUP: 33,
+        PAGEDOWN: 34,
+        END: 35,
+        HOME: 36,
+        LEFTARROW: 37,
+        UPARROW: 38,
+        RIGHTARROW: 39,
+        DOWNARROW: 40
+    };
+
+}(jQuery, window, document));
